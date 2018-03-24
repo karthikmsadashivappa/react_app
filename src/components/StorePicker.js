56e92688e689ff5177117c484947274752458e8a
Karthik M S <ms18karthik@gmail.com>
2018-03-24 15:56:44 +0530

    Creating application layout with components

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -4,9 +4,9 @@ class StorePicker extends React.Component {
 	render() {
 		return (
 		<form className="store-selector">
-		<h2>Please Enter A Store</h2>
-		<input type="text" required placeholder="Store Name" />
-		<button type="submit">Submit -></button>
+			<h2>Please Enter A Store</h2>
+			<input type="text" required placeholder="Store Name" />
+			<button type="submit">Submit -></button>
 		</form>
 		)
 	}
